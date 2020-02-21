39a874386b241b3c39d31aececd296580b754be4
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-21 09:34:48 +0000

    written out component, displaying smurfs list when click on button

--- smurfs/src/components/displaySmurf.js
+++ smurfs/src/components/displaySmurf.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchSmurfs } from './actions/actionCreators';
-// import Loader from 'react-loader-spinner';
+import Loader from 'react-loader-spinner';
 
 function DisplaySmurf(props) {
 
     return (
         <div>
+         <button onClick={props.fetchSmurfs}>Get Smurfs!!</button>
+         {!props.propsSmurfs && !props.propsIsLoading && (
+            <h3>Get a List of Smurfs Here </h3>
+        )}
 
+        {props.propsIsLoading && (
+            <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={300}
+            />
+        )}
 
+                <div>
+                 <h2>Smurfs: </h2>
+                
+                 {props.propsSmurfs && !props.propsIsLoading && (
+                   <div>
+                     {props.propsSmurfs.map(sm => (
+                       <div>
+                     {sm.name}
+                     {sm.age}
+                     {sm.height}
+                        </div>
 
-
-
+                       
+                    ))}
+                   </div>
+                 )}
+             </div>
 
         </div>
     )
