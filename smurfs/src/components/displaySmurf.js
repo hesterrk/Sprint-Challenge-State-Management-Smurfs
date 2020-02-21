c9415944f599baeba779688b283a730660144e3d
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-21 09:31:19 +0000

    connected my smurf reducer state to my displaysmurf component and conected the action creator function

--- smurfs/src/components/displaySmurf.js
+++ smurfs/src/components/displaySmurf.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchSmurfs } from './actions/actionCreators';
 // import Loader from 'react-loader-spinner';
 
 function DisplaySmurf(props) {
@@ -7,10 +8,26 @@ function DisplaySmurf(props) {
     return (
         <div>
 
+
+
+
+
+
         </div>
     )
-
 }
 
 
-export default connect()(DisplaySmurf)
+const mapStateToProps = state => {
+    return {
+        propsSmurfs: state.smurfReducer.smurfs,
+        propsIsLoading: state.smurfReducer.isLoading,
+        propsError: state.smurfReducer.error 
+    };
+};
+
+
+export default connect(
+    mapStateToProps,
+    {fetchSmurfs}
+    )(DisplaySmurf);
