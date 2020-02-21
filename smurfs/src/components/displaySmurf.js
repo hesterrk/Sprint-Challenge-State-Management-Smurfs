9af899614264ed9f826c3250c35577e082b068df
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-21 10:04:27 +0000

    added styled button

--- smurfs/src/components/displaySmurf.js
+++ smurfs/src/components/displaySmurf.js
@@ -2,12 +2,36 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchSmurfs } from './actions/actionCreators';
 import Loader from 'react-loader-spinner';
+import styled from 'styled-components';
+
+const Button = styled.button `
+    box-shadow:inset 0px 1px 0px 0px #e184f3;
+    background:linear-gradient(to bottom, #c123de 5%, #a20dbd 100%);
+	background-color: lavender;
+	border-radius:6px;
+	border:1px solid #a511c0;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+    text-shadow:0px 1px 0px #9b14b3;
+    margin-top: 5px;
+`
+
+
+
+
+
 
 function DisplaySmurf(props) {
 
     return (
         <div>
-         <button onClick={props.fetchSmurfs}>Get Smurfs!!</button>
+         <Button onClick={props.fetchSmurfs}>Get Smurfs!!</Button>
          {!props.propsSmurfs && !props.propsIsLoading && (
             <h3>Get a List of Smurfs Here </h3>
         )}
