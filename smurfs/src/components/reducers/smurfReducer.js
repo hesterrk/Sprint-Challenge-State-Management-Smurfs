5db16459c2ccad8ed08c48c97f0c2a11a0eb89fb
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-21 09:29:30 +0000

    reducers written

--- smurfs/src/components/reducers/smurfReducer.js
+++ smurfs/src/components/reducers/smurfReducer.js
@@ -1,3 +1,4 @@
+import * as types from '../actions/actionTypes';
 
 const initialState = {
     smurfs: [],
@@ -8,7 +9,59 @@ const initialState = {
 
 
 export function smurfReducer(state = initialState, action) {
-return state 
+
+    switch(action.type) {
+
+        case types.FETCHING_SMURF_START :
+            return {
+                ...state,
+                isLoading: true
+            };
+
+            case types.FETCHING_SMURF_SUCCESS :
+                return {
+                    ...state,
+                    isLoading: false,
+                    smurfs: action.payload
+                };
+
+                case types.FETCHING_SMURF_ERROR :
+                    return {
+                        ...state,
+                        error: action.payload
+                    };
+
+
+                case types.POST_SMURF_START :
+                    return {
+                        ...state,
+                        isLoading: true
+                    };
+
+                case types.POST_SMURF_SUCCESS :
+                    return {
+                        ...state,
+                        isLoading: false,
+                        // smurfs: state.smurfs.concat(action.payload)
+                        smurfs: [...state.smurfs, action.payload]
+                    }
+
+                    case types.POST_SMURF_ERROR : 
+                    return {
+                        ...state,
+                        error: action.payload
+                    }
+
+
+                  default :
+                     return state
+    }
+
+
+
+
+
+
 
 
 }
