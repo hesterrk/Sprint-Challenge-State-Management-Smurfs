8a4d9e7b1d360fb07c09f847340240efb274e58c
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-21 09:43:17 +0000

    smurf form written

--- smurfs/src/components/smurfForm.js
+++ smurfs/src/components/smurfForm.js
@@ -6,8 +6,60 @@ import { changeInput, postNewSmurf } from './actions/actionCreators';
 
 function SmurfForm(props) {
 
+    const onChange = e => {
+    props.changeInput({
+            inputName: e.target.name,
+            inputValue: e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        props.postNewSmurf({
+            name: props.propsName,
+            age: props.propsAge,
+            height: props.propsHeight
+        })
+        
+    }
+
+
+    if(props.propsLoader) {
+        return <div>Sending Your Smurf...</div>
+    }
+
+
     return (
         <div>
+           <form onSubmit={onSubmit}>
+        <label> Name:<br></br>
+        <input 
+        value={props.propsName}
+        onChange={onChange}
+        name="name"
+
+        />
+        </label><br></br>
+
+        <label> Age: <br></br>
+        <input 
+        value={props.propsAge}
+        onChange={onChange}
+        name="age"
+
+        />
+
+        </label> <br></br>
+        <label> Height: <br></br>
+        <input 
+        value={props.propsHeight}
+        onChange={onChange}
+        name="height"
+
+        />
+        </label> <br></br>
+        <input type="submit" />
+        </form> 
 
         </div>
     )
